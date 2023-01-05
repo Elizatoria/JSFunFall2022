547f3b0a7abf7f461d83d7877f9cada5df62818a
Elizatoria <Elizatoria@gmail.com>
2023-01-04 21:22:15 -0500

    Finished

--- exercises/26-async-await/async-await.js
+++ exercises/26-async-await/async-await.js
@@ -8,4 +8,16 @@
    * This is the API you will be using. The method will be GET.
    * https://ron-swanson-quotes.herokuapp.com/v2/quotes
    */
+  let getQuote = document.querySelector("#getQuote");
+  
+  getQuote.addEventListener('click', async () => {
+    const response = await axios({
+      url: "https://ron-swanson-quotes.herokuapp.com/v2/quotes",
+      method: "GET",
+      })
+      const data = response.data[0];
+
+      const quote = document.querySelector("#quote");
+      quote.textContent = data;
+  });
 })();
